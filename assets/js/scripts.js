b139283e76e38522cb01bf8f854a4e08a07dd745
Babar <ababar@bmc.com>
2017-03-27 15:24:23 +0530

    Adding map params

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(18.4541863, 73.8477873);
+        var myLatlng = new google.maps.LatLng(18.575348, 73.885980);
 
             var styles = [
                 {
